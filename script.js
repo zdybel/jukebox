a926164b5c62680c843c2764f06cc85696caaad6
Brynne Zdybel <bazdybel@gmail.com>
2016-11-25 21:58:27 -0600

    setup jukebox function and html basic

---
+++ script.js
@@ -0,0 +1,16 @@
+function Jukebox(){
+	this.songs = [];
+	this.play = function (){
+		//play MP3
+	};
+	this.pause = function(){
+		//pause song
+	};
+	this.addSong = function(x){
+		this.songs.push(x)
+	};
+}
+
+function Song(){
+	this.name = name;
+}
